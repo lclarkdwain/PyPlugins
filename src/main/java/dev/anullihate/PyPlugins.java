d96a46e4c3eabe4dff1b2347408c2fc05131683b
anullihate <lclarkdwain@outlook.com>
2020-03-18 18:21:15 +0800

    convert deps to shade

--- src/main/java/dev/anullihate/PyPlugins.java
+++ src/main/java/dev/anullihate/PyPlugins.java
@@ -20,49 +20,6 @@ public class PyPlugins extends PluginBase {
         return instance;
     }
 
-    public void onLoad() {
-        if (!new File("lib/jython-standalone.jar").exists()) {
-            getServer().getLogger().critical("Could not find lib/jython-standalone.jar!");
-            try {
-                URL jythonRepo = new URL("https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.1/jython-standalone-2.7.1.jar");
-                URLConnection connection = jythonRepo.openConnection();
-                connection.connect();
-
-                File libFolder = new File("lib");
-                if (!libFolder.exists()) {
-                    libFolder.mkdir();
-                }
-
-                InputStream inputStream = connection.getInputStream();
-                FileOutputStream fileOutputStream = new FileOutputStream(new File("lib/jython-standalone.jar"));
-
-                long total = connection.getContentLengthLong();
-                long progress = 0;
-                byte[] buffer = new byte[1024];
-                int read;
-                long start = System.currentTimeMillis();
-
-                while ((read = inputStream.read(buffer)) != -1) {
-                    fileOutputStream.write(buffer, 0, read);
-                    progress += read;
-                    if (System.currentTimeMillis() - start > 2000) {
-                        getServer().getLogger().warning("Downloading jython-standalone: " + (progress * 100 / total) + " %");
-                        start = System.currentTimeMillis();
-                    }
-                }
-
-                fileOutputStream.close();
-                inputStream.close();
-
-                getServer().getLogger().info("Download Successful");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            getServer().getLogger().info("lib/jython-standalone.jar found!");
-        }
-    }
-
     public void onEnable() {
         this.getServer().getPluginManager().registerInterface(PyPluginLoader.class);
         List<String> loaders = new ArrayList<>();
